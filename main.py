00337d2204633a6cc30995021843f773c23a5a5d
ahtesham007 <zsyedahtesham@yahoo.com>
2023-03-17 15:28:12 +0530

    update, delete added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional, Union
 from pydantic import BaseModel
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from random import randrange
 
 app = FastAPI()
@@ -14,9 +14,10 @@ class Post(BaseModel):
 my_posts = [{"title":"Test1", "content": "cool", "id":1}, {"title":"Test1", "content": "cool", "id":2}]
 
 def find_post(id):
-    for post in my_posts:
+    for i,post in enumerate(my_posts):
         if id == post["id"]:
-            return post
+            return post, i
+    return None, None
 
 @app.get("/")
 def read_root():
@@ -26,7 +27,7 @@ def read_root():
 def read_posts():
     return {"posts": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(payload: Post):
     post_dict = payload.dict()
     post_dict["id"] = randrange(0, 100000000)
@@ -34,8 +35,36 @@ def create_posts(payload: Post):
     return {"post":post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    post = find_post(id)
+def get_post(id: int, response: Response):
+    post, _ = find_post(id)
+
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message" : "not found"}
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id :{id} was not found")
 
     return {"post" : post}
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    _, ind = find_post(id)
+
+    if ind == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"id:{id} does not exist"
+        )
+
+    my_posts.pop(ind)
+
+@app.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_post(id:int, payload:Post):
+    _, ind = find_post(id)
+    if _ == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"id:{id} does not exist"
+        )
+    post_dict = payload.dict()
+    post_dict["id"] = id
+    my_posts[ind] = post_dict
+    return {"detail": post_dict}

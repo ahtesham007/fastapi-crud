02f680c4de9edf5ac238d72e8a99a211cfb9546b
ahtesham007 <zsyedahtesham@yahoo.com>
2023-03-17 14:23:10 +0530

    feat: get all, get by id, create new post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional, Union
 from pydantic import BaseModel
 from fastapi import Body, FastAPI
+from random import randrange
 
 app = FastAPI()
 
@@ -10,15 +11,31 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title":"Test1", "content": "cool", "id":1}, {"title":"Test1", "content": "cool", "id":2}]
+
+def find_post(id):
+    for post in my_posts:
+        if id == post["id"]:
+            return post
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/posts")
+def read_posts():
+    return {"posts": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(payload: Post):
-    print(payload.dict())
-    return {"new_post":payload}
+    post_dict = payload.dict()
+    post_dict["id"] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return {"post":post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+
+    return {"post" : post}
+

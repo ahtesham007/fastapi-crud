a74f79a359c5bdd4d9dc5210be2cb5f49b01bed4
ahtesham007 <zsyedahtesham@yahoo.com>
2023-03-20 21:04:22 +0530

    added multiple scenarios for incorrect login test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 from app import schemas
 from app.config import settings
 from jose import jwt
+import pytest
 
 def test_root(client):
     res = client.get('/')
@@ -28,7 +29,14 @@ def test_login_user(client, test_user):
     assert login_res.token_type == 'bearer '
     assert res.status_code == 200
 
-def test_incorrect_login(client, test_user):
-    res = client.post('/login', data={"username": test_user['email'], "password": 'password'})
-    assert res.status_code == 403
-    assert res.json().get('detail') == 'Invalid Credentials'
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+])
+def test_incorrect_login(client, test_user, email, password, status_code):
+    res = client.post('/login', data={"username": email, "password": password})
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'

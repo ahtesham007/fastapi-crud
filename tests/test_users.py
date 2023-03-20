74ca61b739d5e86c266833e52f4a5bdddaedb7d2
ahtesham007 <zsyedahtesham@yahoo.com>
2023-03-20 20:59:33 +0530

    added incorrect login test

--- tests/test_users.py
+++ tests/test_users.py
@@ -27,3 +27,8 @@ def test_login_user(client, test_user):
     assert id == test_user['id']
     assert login_res.token_type == 'bearer '
     assert res.status_code == 200
+
+def test_incorrect_login(client, test_user):
+    res = client.post('/login', data={"username": test_user['email'], "password": 'password'})
+    assert res.status_code == 403
+    assert res.json().get('detail') == 'Invalid Credentials'

c427bcf01cf92faa7dc9fbfafdf06933d436eeda
ahtesham007 <zsyedahtesham@yahoo.com>
2023-03-21 11:21:39 +0530

    added test for posts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from app import models
 from app.database import get_db, Base
 from app.main import app
 from urllib.parse import quote_plus
@@ -8,6 +9,8 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 import pytest
 from alembic import command
+
+from app.oauth2 import create_access_token
 
 user = settings.user
 encoded_pwd = quote_plus(settings.pwd)
@@ -54,3 +57,63 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data["password"]
     return new_user
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "sanj@gmail.com",
+                 "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user2['id']
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
+    session.commit()
+
+    posts = session.query(models.Post).all()
+    return posts
